5bf3c9949017b0221efa5fdf60f1d9f6081d6885
Siarhei Busiel <sju.busel@gmail.com>
2020-04-23 18:15:10 +0300

    add LoginFilter.java to servlets ("/resources") in web-module

---
+++ login/web/src/main/java/com/github/sjubusel/studying/login/filter/LoginFilter.java
@@ -0,0 +1,34 @@
+package com.github.sjubusel.studying.login.filter;
+
+import com.github.sjubusel.studying.login.Util;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(value = {"/resources"})
+public class LoginFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        Cookie[] cookies = request.getCookies();
+        Cookie cookie = Util.getCookieIfExists(cookies, "logged");
+        if (cookie != null && Util.ifCookieIsValid(cookie)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            Util.sendRedirect("login.jsp", request, response);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
