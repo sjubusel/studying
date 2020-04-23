e8866774b26537a97ca8350b8fccad424d162f4a
Siarhei Busiel <sju.busel@gmail.com>
2020-04-23 13:12:35 +0300

    add Util.java in web-module

--- login/web/src/main/java/com/github/sjubusel/studying/login/Util.java
+++ login/web/src/main/java/com/github/sjubusel/studying/login/Util.java
@@ -4,6 +4,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 public class Util {
     private Util() {
@@ -12,5 +15,11 @@ public class Util {
     public static void forwardToJsp(String jspName, HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.getRequestDispatcher("/" + jspName + ".jsp").forward(request, response);
+    }
+
+    public static String getDateTimeBeBy(HttpServletRequest request) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG, FormatStyle.LONG)
+                .withLocale(request.getLocale());
+        return ZonedDateTime.now().format(dateTimeFormatter);
     }
 }
