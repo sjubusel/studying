087ccbe1013d01eb42be85c064e44a93dfb15acb
Siarhei Busiel <sju.busel@gmail.com>
2020-04-23 18:13:54 +0300

    add empty servlets ("/login", "/index", "/resources") in web-module

---
+++ login/web/src/main/java/com/github/sjubusel/studying/login/servlet/IndexServlet.java
@@ -0,0 +1,22 @@
+package com.github.sjubusel.studying.login.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/index")
+public class IndexServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        super.doGet(req, resp);
+    }
+}
---
+++ login/web/src/main/java/com/github/sjubusel/studying/login/servlet/LoginServlet.java
@@ -0,0 +1,18 @@
+package com.github.sjubusel.studying.login.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = {"/login"})
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+
+    }
+}
