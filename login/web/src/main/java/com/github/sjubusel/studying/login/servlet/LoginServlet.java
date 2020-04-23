8aac86bce002f0b17d87f8eac7162f3193afb696
Siarhei Busiel <sju.busel@gmail.com>
2020-04-23 19:01:48 +0300

    add doGet and doPost functionality to "/login"-servlet (web)

--- login/web/src/main/java/com/github/sjubusel/studying/login/servlet/LoginServlet.java
+++ login/web/src/main/java/com/github/sjubusel/studying/login/servlet/LoginServlet.java
@@ -1,7 +1,13 @@
 package com.github.sjubusel.studying.login.servlet;
 
+import com.github.sjubusel.studying.login.AuthUser;
+import com.github.sjubusel.studying.login.AuthUserService;
+import com.github.sjubusel.studying.login.Util;
+import com.github.sjubusel.studying.login.impl.DefAuthUserService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,10 +15,33 @@ import java.io.IOException;
 
 @WebServlet(value = {"/login"})
 public class LoginServlet extends HttpServlet {
+    private AuthUserService authUserService = DefAuthUserService.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (Util.getCookieIfExists(req.getCookies(), "logged") == null) {
+            Util.forwardToJsp("login", req, resp);
+        }
+        Util.sendRedirect("index", req, resp);
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String login = req.getParameter("login");
         String password = req.getParameter("password");
+        AuthUser user = authUserService.login(login, password);
+        if (user != null) {
+            resp.addCookie(createLongTimeCookie(user.getUserId()));
+            Util.sendRedirect("/resources", req, resp);
+        } else {
+            req.setAttribute("error", "login or password invalid");
+            Util.forwardToJsp("login", req, resp);
+        }
+    }
 
+    private Cookie createLongTimeCookie(String value) {
+        Cookie cookie = new Cookie("logged", value);
+        cookie.setMaxAge(24 * 60 * 60);
+        return cookie;
     }
 }
