043b7bc62a29748464b719b3c7edc07e1e5a8285
Siarhei Busiel <sju.busel@gmail.com>
2020-04-24 10:11:01 +0300

    Add correct doPost'method to "/register"-servlet (web: RegisterServlet.java)

--- login/web/src/main/java/com/github/sjubusel/studying/login/servlet/RegisterServlet.java
+++ login/web/src/main/java/com/github/sjubusel/studying/login/servlet/RegisterServlet.java
@@ -1,6 +1,9 @@
 package com.github.sjubusel.studying.login.servlet;
 
+import com.github.sjubusel.studying.login.AuthUser;
+import com.github.sjubusel.studying.login.AuthUserService;
 import com.github.sjubusel.studying.login.Util;
+import com.github.sjubusel.studying.login.impl.DefAuthUserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +14,8 @@ import java.io.IOException;
 
 @WebServlet(value = {"/register"})
 public class RegisterServlet extends HttpServlet {
+    private AuthUserService authUserService = DefAuthUserService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (Util.getCookieIfExists(req.getCookies(), "logged") == null) {
@@ -21,6 +26,15 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        AuthUser user = authUserService.register(login, password);
+        if (user != null) {
+            resp.addCookie(Util.createLongTimeCookie(user.getUserId()));
+            Util.sendRedirect("/resources", req, resp);
+        } else {
+            req.setAttribute("error", "login is already used by another user");
+            Util.forwardToJsp("register", req, resp);
+        }
     }
 }
