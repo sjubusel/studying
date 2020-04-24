dbfd50531ce28ede51752a5160e70d8606b4be96
Siarhei Busiel <sju.busel@gmail.com>
2020-04-24 17:12:45 +0300

    Prepare news articles in order to view in register.jsp (web: ResourcesServlet.java)

--- login/web/src/main/java/com/github/sjubusel/studying/login/servlet/ResourcesServlet.java
+++ login/web/src/main/java/com/github/sjubusel/studying/login/servlet/ResourcesServlet.java
@@ -11,7 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
+import java.time.ZonedDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @WebServlet(value = {"/resources"})
 public class ResourcesServlet extends HttpServlet {
@@ -19,8 +21,14 @@ public class ResourcesServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<NewsArticle> newsArticles = newArticleService.getNewsArticles();
-        req.setAttribute("articles", newsArticles);
+        Map<ZonedDateTime, NewsArticle> newsArticles = newArticleService.getNewsArticles();
+        Map<String, NewsArticle> articles = new LinkedHashMap<>();
+        for (Map.Entry<ZonedDateTime, NewsArticle> entry : newsArticles.entrySet()) {
+            ZonedDateTime zonedDateTime = entry.getKey();
+            String localeDateTime = Util.getZonedDateTimeToString(zonedDateTime, req.getLocale());
+            articles.put(localeDateTime, entry.getValue());
+        }
+        req.setAttribute("articles", articles);
         Util.forwardToJsp("resources", req, resp);
     }
 }
