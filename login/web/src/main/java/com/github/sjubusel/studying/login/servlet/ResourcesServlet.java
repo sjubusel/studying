f7ee11be32960bfdd94a8977f2d1cf8307f23b4c
Siarhei Busiel <sju.busel@gmail.com>
2020-04-24 15:01:16 +0300

    Clean imports in LoginServlet.java (web) and insert service functionality to "/resources"'s doGet method

--- login/web/src/main/java/com/github/sjubusel/studying/login/servlet/ResourcesServlet.java
+++ login/web/src/main/java/com/github/sjubusel/studying/login/servlet/ResourcesServlet.java
@@ -1,16 +1,26 @@
 package com.github.sjubusel.studying.login.servlet;
 
+import com.github.sjubusel.studying.login.NewArticleService;
+import com.github.sjubusel.studying.login.NewsArticle;
+import com.github.sjubusel.studying.login.Util;
+import com.github.sjubusel.studying.login.impl.DefNewsArticleService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(value = {"/resources"})
 public class ResourcesServlet extends HttpServlet {
+    private NewArticleService newArticleService = DefNewsArticleService.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        List<NewsArticle> newsArticles = newArticleService.getNewsArticles();
+        req.setAttribute("articles", newsArticles);
+        Util.forwardToJsp("resources", req, resp);
     }
 }
