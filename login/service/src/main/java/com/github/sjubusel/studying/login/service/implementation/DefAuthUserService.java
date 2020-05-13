5f0fc4697f64cc5e75fd3263444456721925f0e0
Siarhei Busiel <sju.busel@gmail.com>
2020-05-13 10:38:57 +0300

    Add logger statements to DefAuthUserService.java (service)

--- login/service/src/main/java/com/github/sjubusel/studying/login/service/implementation/DefAuthUserService.java
+++ login/service/src/main/java/com/github/sjubusel/studying/login/service/implementation/DefAuthUserService.java
@@ -5,10 +5,13 @@ import com.github.sjubusel.studying.login.dao.AuthUserDao;
 import com.github.sjubusel.studying.login.service.AuthUserService;
 import com.github.sjubusel.studying.login.model.Role;
 import com.github.sjubusel.studying.login.dao.impl.DefAuthUserDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.UUID;
 
 public class DefAuthUserService implements AuthUserService {
+    private static final Logger logger = LoggerFactory.getLogger(DefAuthUserService.class);
     private AuthUserDao authUserDao = DefAuthUserDao.getInstance();
 
     private static volatile AuthUserService instance;
@@ -30,16 +33,17 @@ public class DefAuthUserService implements AuthUserService {
     public AuthUser login(String login, String password) {
         AuthUser user = authUserDao.getByLogin(login);
         if (user != null && user.getPassword().equals(password)) {
+            logger.info("User \"{}\" successfully logged", login);
             return user;
         } else {
+            logger.info("User \"{}\" failed to login with a password \"{}\"", login, password);
             return null;
         }
     }
 
     @Override
     public AuthUser login(String userId) {
-        authUserDao.getByUserId(userId);
-        return null;
+        return authUserDao.getByUserId(userId);
     }
 
     @Override
@@ -53,6 +57,7 @@ public class DefAuthUserService implements AuthUserService {
             authUserDao.saveAuthUser(new AuthUser(login, password, Role.USER, UUID.randomUUID().toString()));
             return authUserDao.getByLogin(login);
         } else {
+            logger.info("Already occupied login \"{}\" is used while registration. Password \"{}\"", login, password);
             return null;
         }
     }
